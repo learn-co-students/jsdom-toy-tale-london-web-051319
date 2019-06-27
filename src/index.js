abacdeebec6ca3d2f4ce80cc6e18a264f2236689
James <hayward.jp@gmail.com>
2019-06-27 16:19:28 +0100

    Completed some of the tasks

--- src/index.js
+++ src/index.js
@@ -16,4 +16,42 @@ addBtn.addEventListener('click', () => {
 })
 
 
-// OR HERE!
+document.addEventListener('DOMContentLoaded', function(e) {
+   init();
+});
+
+const BASE_URL = 'http://localhost:3000/toys';
+
+let init = () => {
+   fetch(BASE_URL)
+   .then(data => data.json())
+   .then(jsonData => display(jsonData));
+};
+
+let display = (jsonData) => {
+   const container = document.querySelector('#toy-collection');
+   for(i = 0; i < jsonData.length; i++) {
+      let newCard = document.createElement('div');
+      newCard.className = 'card';
+      let newHeader = document.createElement('h2');
+      newHeader.innerText = jsonData[i].name;
+      let newImg = document.createElement('img');
+      newImg.src = jsonData[i].image;
+      newImg.className = 'toy-avatar'
+      let newLikes = document.createElement('p');
+      newLikes.innerHTML = jsonData[i].likes;
+      let newButton = document.createElement('button');
+      newButton.innerText = 'Like!';
+      newButton.addEventListener('click', function(e) {
+         like(jsonData[i]);
+      });
+      newCard.append(newHeader, newImg, newLikes, newButton);
+      container.append(newCard);
+   }
+};
+
+let like = (object) => {
+   debugger
+};
+
+// POST SECTION
